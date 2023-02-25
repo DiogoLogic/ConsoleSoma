cfe5e5f109b62797cec444984c9baf44d3bd9a46
Emmanoel <Emmanoel@DESKTOP-6EM3HUE>
2023-02-24 22:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o numero a ser somado: ");
+
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"O resutado da somá é {Soma(numero1, numero2)}");
+
+
+            Console.WriteLine("Aperte Enter para Finaizar");
+            Console.ReadLine();
+        }
+
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+    }
+}
